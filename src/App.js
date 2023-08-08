d78e8a1c8c79df04522c515f6820d0bb2eddc2ee
The princetoutcouleur <babathiam0000@gmail.com>
2023-08-08 13:18:05 +0000

    page de connection

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Admin from './components/admin/Admin';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Dashboard from './components/Dashboard/dashboard'
+import Login from './components/login/Login';
 function App() {
   return (
     <div className="App">
       {/* <Admin/> */}
-      <Dashboard  />
+      {/* <Dashboard  /> */}
+      <Login />
       
     </div>
   );
