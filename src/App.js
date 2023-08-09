10e7ec7ce8ecd5128d94c0470a987c48da691c52
saliou thioune <bayezalle995@gmail.com>
2023-08-09 09:28:59 +0000

    livraison

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
 import Admin from './components/admin/Admin';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Dashboard from './components/Dashboard/dashboard'
@@ -7,6 +7,7 @@ import Login from './components/login/Login';
 import ForgotPW from './components/ForgotPW/ForgotPW'
 import PageAccueilCoach from './components/pageAccueilCoach/PageAccueilCoach';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Livraison from './components/Livraisons/Livraison';
 function App() {
   return (
     <div className="App">
@@ -15,6 +16,7 @@ function App() {
       {/* <Login /> */}
       <ForgotPW/>
       {/* <PageAccueilCoach /> */}
+      {/* <Livraison /> */}
       
     </div>
   );
