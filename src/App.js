ec69b613b4f1c80ba079f8cda9b0bd12ccd7dbe6
yagou-tech <yagueba2013@gmail.com>
2023-08-14 16:36:16 +0000

    hdsf

--- src/App.js
+++ src/App.js
@@ -2,29 +2,34 @@ import './App.css';
 import 'bootstrap/dist/js/bootstrap.min.js'
 import Admin from './components/ComponentAdmin/admin/Admin';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Dashboard from './components/ComponentCoach/Dashboard/dashboard'
 import Login from './components/ComponentCoach/login/Login';
 import ForgotPW from './components/ComponentCoach/ForgotPW/ForgotPW'
+import SideBar from './components/ComponentCoach/SideBar/Sidebar';
+import Navbar from './components/ComponentCoach/NavBar/Navbar';
+import Layout from './components/ComponentCoach/Layout/Layout';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 
 
 // import Domain from './components/Domain/Domain';
 // import Programmation from './components/Domain/Programmation';
 // import Marketing from './components/Domain/Marketing';
 // import Design from './components/Domain/Design';
-import Domain from './components/Domain/Coach/Domain';
-import Programmation from './components/Domain/Coach/Programmation';
-import Marketing from './components/Domain/Coach/Marketing';
-import Design from './components/Domain/Coach/Design';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import Domain from './components/ComponentCoach/Domain/Coach/Domain';
+// import Programmation from './components/ComponentCoach/Domain/Coach/Programmation';
+// import Marketing from './components/ComponentCoach/Domain/Coach/Marketing';
+// import Design from './components/ComponentCoach/Domain/Design';
 
 import Chat from './components/ComponentCoach/Chat/Chat.jsx';
-
 import Livraison from './components/ComponentCoach/Livraisons/Livraison';
+import Dashboards from './components/ComponentCoach/Dashboard/Dashboards';
+import Domain from './components/ComponentCoach/Domain/Domain';
+
 function App() {
   return (
     <div className="App">
       {/* <Admin/> */}
-      <Dashboard  />
+      {/* <Dashboard  /> */}
       
       {/* <Login /> */}
       {/* <ForgotPW/> */}
@@ -34,11 +39,20 @@ function App() {
       {/* <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Domain />} />
-          <Route path="/" element={<Programmation />} />
-          <Route path="/" element={<Marketing />} />
-          <Route path="/" element={<Design />} />
+          <Route path="/programmation" element={<Programmation />} />
+          <Route path="/marketing" element={<Marketing />} />
+          <Route path="/design" element={<Design />} />
         </Routes>
       </BrowserRouter> */}
+      <Router>
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path="/layout" element={ <Layout />}>
+            <Route path='/layout/dashboard' element={<Dashboards />} />
+            <Route path='/layout/dashboard/domain' element={<Domain />} />
+          </Route>
+        </Routes>
+    </Router>
 
      
     </div>
---
+++ src/components/ComponentCoach/Layout/Layout.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Sidebar from '../SideBar/Sidebar';
+import Navbar from '../NavBar/Navbar';
+import { Outlet } from 'react-router-dom';
+
+const Layout = () => {
+  return (
+    <>
+        <div className='container-fluid' >
+            <div className='row'>
+                <div className='col-2 min-vh-100 bg-white'>
+                  <Sidebar />
+                </div>
+                <div className='col-10'>
+                    <div className='row bg-primary-subtle '>
+                      <Navbar />
+                    </div>
+                    <Outlet />
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Layout
