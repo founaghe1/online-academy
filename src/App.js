b8200e04d711347c537cdbd22fcac7cc9c212ea3
Mohamed Diallo <mohameddiallo.msd11@gmail.com>
2023-08-15 15:19:56 +0000

    DomApprnt

--- src/App.js
+++ src/App.js
@@ -8,38 +8,30 @@ import SideBar from './components/ComponentCoach/SideBar/Sidebar';
 import Navbar from './components/ComponentCoach/NavBar/Navbar';
 import Layout from './components/ComponentCoach/Layout/Layout';
 
-// import Domain from './components/Domain/Domain';
-// import Programmation from './components/Domain/Programmation';
-// import Marketing from './components/Domain/Marketing';
-// import Design from './components/Domain/Design';
-
-// import Domain from './components/ComponentCoach/Domain/Coach/Domain';
-// import Programmation from './components/ComponentCoach/Domain/Coach/Programmation';
-// import Marketing from './components/ComponentCoach/Domain/Coach/Marketing';
-// import Design from './components/ComponentCoach/Domain/Design';
-
-// import Domain from './components/ComponentCoach/Domain/Domain';
-// import Programmation from './components/ComponentCoach/Domain/Programmation';
-// import Marketing from './components/ComponentCoach/Domain/Marketing';
-// import Design from './components/ComponentCoach/Domain/Design';
+// Coach
+import Domain from './components/ComponentCoach/Domain/Domain';
+import Programmation from './components/ComponentCoach/Domain/Programmation';
+import Marketing from './components/ComponentCoach/Domain/Marketing';
+import Design from './components/ComponentCoach/Domain/Design';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Chat from './components/ComponentCoach/Chat/Chat.jsx';
 import Livraison from './components/ComponentCoach/Livraisons/Livraison';
 import AjoutCours from './components/ComponentCoach/AjoutCours/AjoutCours';
 
+// Apprenants
 import StudentDashboard from './components/ComponentApprenant/StudentDashboard/StudentDashboard';
 import Dashboards from './components/ComponentCoach/Dashboard/Dashboards';
-import Domain from './components/ComponentApprenant/DomainApprnt/Domain';
-import Programmation from './components/ComponentApprenant/DomainApprnt/Programmation';
-import Marketing from './components/ComponentApprenant/DomainApprnt/Marketing';
-import Design from './components/ComponentApprenant/DomainApprnt/Design';
+import DomainApprt from './components/ComponentApprenant/DomainApprnt/DomainApprt';
+import ProgrammationApprt from './components/ComponentApprenant/DomainApprnt/ProgrammationApprt';
+import MarketingApprt from './components/ComponentApprenant/DomainApprnt/MarketingApprt';
+import DesignApprt from './components/ComponentApprenant/DomainApprnt/DesignApprt';
 
 
 function App() {
   return (
     <div className="App">
-      {/* <Admin/> */}
+      {/* <Admin /> */}
       {/* <StudentDashboard /> */}
       
       {/* <Login /> */}
@@ -51,26 +43,26 @@ function App() {
       {/* <Chat/> */}
       
       
-      <Router>
+      {/* <Router>
         <Routes>
           <Route exact path="/" element={<Domain />} />
           <Route path="/programmation" element={<Programmation />} />
           <Route path="/marketing" element={<Marketing />} />
           <Route path="/design" element={<Design />} />
         </Routes>
-      </Router>
+      </Router> */}
 
       {/* Routes Yagouba */}
 
-      {/* <Router>
+      <Router>
         <Routes>
           <Route path='/' element={<Login />} />
           <Route path="/layout" element={ <Layout />}>
             <Route path='/layout/dashboard' element={<Dashboards />} />
-            <Route path='/layout/dashboard/domain' element={<Domain />} />
+            <Route path='/layout/dashboard/domain' element={<DomainApprt />} />
           </Route>
         </Routes>
-      </Router> */}
+      </Router>
 
       {/* <Dashboard  /> */}
       {/* <Login /> */}
