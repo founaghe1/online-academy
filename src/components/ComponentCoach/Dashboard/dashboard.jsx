5259ceb6b982551421611a1369424178c62d8290
The princetoutcouleur <babathiam0000@gmail.com>
2023-08-16 10:14:21 +0000

    new coach dashboard

--- src/components/ComponentCoach/Dashboard/dashboard.jsx
+++ src/components/ComponentCoach/Dashboard/dashboard.jsx
@@ -1,31 +1,29 @@
 import React from 'react'
 import NavBar from '../NavBar/Navbar'
-import SideBar from '../SideBar/sidebar'
+import SideBar from '../SideBar/Sidebar'
 import Modale from '../Cour/Modal'
 import Coure from '../Cour/Cour'
 import PageAccueilCoach from '../pageAccueilCoach/PageAccueilCoach'
 const dashboard = () => {
   return (
     <>
-    
-        <div className='container-fluid' >
-            <div className='row'>
-                <div className='col-2 min-vh-100 bg-white'>
-                    <SideBar />
-                </div>
-                <div className='col-10'>
-                    <div className='row bg-primary-subtle '>
-                        <NavBar />
-                        <PageAccueilCoach />
-
-                        <Modale />
-                        <Coure />
-                    </div>
-                </div>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-lg-2">
+            <SideBar />
+          </div>
+          <div className="col-lg-10">
+            <div className="row bg-primary-subtle ">
+              <NavBar />
+              <PageAccueilCoach />
+              <Modale />
+              <Coure />
             </div>
+          </div>
         </div>
+      </div>
     </>
-  )
+  );
 }
 
 export default dashboard
