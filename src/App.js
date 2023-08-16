fb180e09b7dec89c73836f83d1ede40792527071
yaacinediop <yacinediopkebe@gmail.com>
2023-08-16 11:38:28 +0000

    notifcoach

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
       {/* <Livraison /> */}
       {/* <Chat/> */}
       {/* <ListeApprenants/> */}
-      <NotificationsCoachs/>
+      {/* <NotificationsCoachs/> */}
       
       
       {/* <BrowserRouter>
--- src/components/ComponentCoach/NotificationsCoachs/NotificationsCoachs.jsx
+++ src/components/ComponentCoach/NotificationsCoachs/NotificationsCoachs.jsx
@@ -1,14 +1,54 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import { useState } from 'react';
+
 import './NotificationsCoachs.css'
 const NotificationsCoachs = () => {
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <>
        <div
       className="modal show"
       style={{ display: 'block', position: 'initial' }}
     >
+       <button className="btn btn-secondary text-white add"
+                      onClick={handleShow}>
+                            
+                    </button>
+
+
+                    <Modal show={show} onHide={handleClose}>
+                      <Modal.Header closeButton>
+                        <Modal.Title>
+                          <h3>Modifier le profil</h3>
+                        </Modal.Title>
+                      </Modal.Header>
+                      <Modal.Body className="my-3">
+                        
+                      </Modal.Body>
+                      <Modal.Footer>
+                        <Button
+                          className="btn btn-secondary text-white fw-bold"
+                          onClick={handleClose}
+                        >
+                          Fermer
+                        </Button>
+                        <Button
+                          className="btn btn-secondary text-white save"
+                          onClick={handleClose}
+                        >
+                          Enregistrer
+                        </Button>
+                      </Modal.Footer>
+                    </Modal>
+       <Button variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </Button>
       <Modal.Dialog>
         <Modal.Header closeButton>
           <Modal.Title>Notifications</Modal.Title>
